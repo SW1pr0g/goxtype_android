f338a17f9d27b4b121fd9375f3c5b989241b26d3
Alex Poryadin <poryadin_alex@mail.ru>
2023-03-03 18:07:53 +0300

    add default

---
+++ app/src/main/java/com/sw1pr0g/goxtype_android/TypingTestActivity.kt
@@ -0,0 +1,16 @@
+package com.sw1pr0g.goxtype_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TypingTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_typing_test)
+
+
+
+
+
+    }
+}
